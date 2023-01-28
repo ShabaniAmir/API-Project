4710d19d499022e3ca6a44e57463dec20de1a46f
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-27 19:20:59 -0500

    Added get all events endpoint

--- authenticate-me/backend/routes/api/index.js
+++ authenticate-me/backend/routes/api/index.js
@@ -3,6 +3,7 @@ const sessionRouter = require("./session.js");
 const UsersRouter = require("./users.js");
 const GroupsRouter = require("./groups.js");
 const { restoreUser } = require("../../utils/auth.js");
+const { Event } = require("../../db/models");
 
 // Connect restoreUser middleware to the API router
 // If current user session is valid, set req.user to the user in the database
@@ -14,6 +15,13 @@ router.use("/session", sessionRouter);
 router.use("/users", UsersRouter);
 
 router.use("/groups", GroupsRouter);
+
+// Get all events
+// GET /api/events
+router.get("/events", async (req, res) => {
+  const events = await Event.findAll();
+  return res.json(events);
+});
 
 router.post("/test", (req, res) => {
   res.json({ requestBody: req.body });
