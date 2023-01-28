87717a7c84b738010ba959a0b7725517a8972c3e
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-27 19:29:56 -0500

    Added endpoint to get event by id

--- authenticate-me/backend/routes/api/events.js
+++ authenticate-me/backend/routes/api/events.js
@@ -10,6 +10,7 @@ const { requireAuth } = require("../../auth");
 // Get is handled in index.js
 
 // Get all events of a group
+// GET /api/groups/:groupId/events/
 router.get("/", async (req, res, next) => {
   const { groupId } = req.params;
   const events = await Event.findAll({
@@ -27,6 +28,31 @@ router.get("/", async (req, res, next) => {
   });
 
   return res.json(events);
+});
+
+// Get a single event
+// GET /api/groups/:groupId/events/:eventId
+router.get("/:eventId", async (req, res, next) => {
+  const { eventId, groupId } = req.params;
+  if (!eventId) {
+    const err = new Error("Event ID is required");
+    err.status = 400;
+    err.title = "Event ID is required";
+    err.errors = ["Event ID is required"];
+    return next(err);
+  }
+  const event = await Event.findByPk(eventId, {
+    include: [
+      {
+        model: Venue,
+      },
+      {
+        model: Group,
+      },
+    ],
+  });
+
+  return res.json(event);
 });
 
 module.exports = router;
