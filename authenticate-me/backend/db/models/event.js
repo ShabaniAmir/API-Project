9c1f172d20af887b91b4a6b11bbfa3618a0be2a4
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-27 19:14:02 -0500

    Created Events model and schema

---
+++ authenticate-me/backend/db/models/event.js
@@ -0,0 +1,33 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Event extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.Event.belongsTo(models.Group, { foreignKey: "groupId" });
+      models.Event.belongsTo(models.Venue, { foreignKey: "venueId" });
+    }
+  }
+  Event.init(
+    {
+      groupId: DataTypes.INTEGER,
+      venueId: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+      },
+      name: DataTypes.STRING,
+      type: DataTypes.STRING,
+      startDate: DataTypes.STRING,
+      endDate: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Event",
+    }
+  );
+  return Event;
+};
