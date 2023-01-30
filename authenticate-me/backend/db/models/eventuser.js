ee6c1b073ac7db2bec5398f330b8ea42c41a3572
Amir Shabani <shabani.a.amir@gmail.com>
2023-01-30 08:06:38 -0500

    Added EventUser model

---
+++ authenticate-me/backend/db/models/eventuser.js
@@ -0,0 +1,26 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class EventUser extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      models.EventUser.belongsTo(models.Event, { foreignKey: "eventId" });
+      models.EventUser.belongsTo(models.User, { foreignKey: "userId" });
+    }
+  }
+  EventUser.init(
+    {
+      eventId: { type: DataTypes.INTEGER, references: { model: "Events" } },
+      userId: { type: DataTypes.INTEGER, references: { model: "Users" } },
+    },
+    {
+      sequelize,
+      modelName: "EventUser",
+    }
+  );
+  return EventUser;
+};
